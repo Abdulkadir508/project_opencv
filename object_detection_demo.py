9f27b1800b7f8c1ea9b4a4f5fcc9694d8e7ecf8d
Abdulkadir508 <34213@edu.campusdetandem.be>
2025-05-16 11:00:28 +0200

    Update object_detection_demo.py

--- object_detection_demo.py
+++ object_detection_demo.py
@@ -1,9 +1,12 @@
 import cv2 as cv
 import numpy as np
+import time
 
-cap = cv.VideoCapture(0)  
-prevCircles = None
-dist = lambda x1, y1, x2, y2: ((x1 - x2) ** 2 + (y1 - y2) ** 2)
+cap = cv.VideoCapture(0)
+prev_pos = None
+smoothed_pos = None
+alpha = 0.9
+dist = lambda x1, y1, x2, y2: ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
 while True:
     ret, frame = cap.read()
@@ -15,21 +18,26 @@ while True:
 
     circles = cv.HoughCircles(blurFrame, cv.HOUGH_GRADIENT, 1.2, 100,
                                param1=100, param2=30, minRadius=5, maxRadius=30)
-    if circles is not None:  
-        circles = np.uint16(np.around(circles))  
-        chosen = None
-        for i in circles[0, :]:
-            if chosen is None:
-                chosen = i
-            if prevCircles is not None:
-                if dist(chosen[0], chosen[1], i[0], i[1]) > dist(prevCircles[0], prevCircles[1], i[0], i[1]):
-                    chosen = i
-            cv.circle(frame, (chosen[0], chosen[1]), 1, (0, 100, 100), 3)
-            cv.circle(frame, (chosen[0], chosen[1]), chosen[2], (255, 0, 255),3)
-            prevCircles = chosen
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        chosen = circles[0, 0]  
+
+        
+        if smoothed_pos is None:
+            smoothed_pos = chosen[:2].astype(float)
+        else:
+            smoothed_pos = alpha * chosen[:2] + (1 - alpha) * smoothed_pos
+
+        
+        cv.circle(frame, (int(smoothed_pos[0]), int(smoothed_pos[1])), 1, (0, 100, 100), 3)
+        cv.circle(frame, (int(smoothed_pos[0]), int(smoothed_pos[1])), chosen[2], (255, 0, 255), 3)
+
+        
+        
+
     cv.imshow('Circles', frame)
     if cv.waitKey(1) == ord('q'):
         break
 
-cap.release()  
+cap.release()
 cv.destroyAllWindows()
